2903bc47a29df8da5a11e5faf0ba169672394342
Johan Solbakken <johsol@stud.ntnu.no>
2024-03-26 14:59:07 +0100

    Simple parsing

--- src/queryprocessing/ddl.rs
+++ src/queryprocessing/ddl.rs
@@ -1,4 +1,4 @@
-use std::thread::current;
+use tracing::field;
 
 #[derive(Debug, PartialEq)]
 enum DDLTokenType {
@@ -16,7 +16,6 @@ enum DDLTokenType {
 
 struct DDLToken {
     token_type: DDLTokenType,
-    value: String,
     position: usize,
 }
 
@@ -65,55 +64,47 @@ impl DDLLexer {
 
             token = Some(DDLToken {
                 token_type,
-                value,
                 position: self.position,
             });
         } else if current_char == '@' {
             token = Some(DDLToken {
                 token_type: DDLTokenType::AtSign,
-                value: current_char.to_string(),
                 position: self.position,
             });
             self.position += 1;
         } else if current_char == '?' {
             token = Some(DDLToken {
                 token_type: DDLTokenType::QuestionMark,
-                value: current_char.to_string(),
                 position: self.position,
             });
             self.position += 1;
         } else if current_char == ',' {
             token = Some(DDLToken {
                 token_type: DDLTokenType::Comma,
-                value: current_char.to_string(),
                 position: self.position,
             });
             self.position += 1;
         } else if current_char == '(' {
             token = Some(DDLToken {
                 token_type: DDLTokenType::OpenParen,
-                value: current_char.to_string(),
                 position: self.position,
             });
             self.position += 1;
         } else if current_char == ')' {
             token = Some(DDLToken {
                 token_type: DDLTokenType::CloseParen,
-                value: current_char.to_string(),
                 position: self.position,
             });
             self.position += 1;
         } else if current_char == '{' {
             token = Some(DDLToken {
                 token_type: DDLTokenType::OpenBrace,
-                value: current_char.to_string(),
                 position: self.position,
             });
             self.position += 1;
         } else if current_char == '}' {
             token = Some(DDLToken {
                 token_type: DDLTokenType::CloseBrace,
-                value: current_char.to_string(),
                 position: self.position,
             });
             self.position += 1;
@@ -127,7 +118,170 @@ impl DDLLexer {
     }
 }
 
-struct DDLParser {}
+#[derive(Debug, PartialEq)]
+struct Model {
+    name: String,
+    fields: Vec<Field>,
+}
+
+#[derive(Debug, PartialEq)]
+enum FieldType {
+    Int,
+    Float,
+    String,
+    Date,
+    Blob,
+}
+
+#[derive(Debug, PartialEq)]
+struct Field {
+    name: String,
+    field_type: FieldType,
+    is_nullable: bool,
+    is_primary_key: bool,
+    is_foreign_key: bool,
+    references: Option<(String, String)>,
+}
+
+struct DDLParser {
+    tokens: Vec<DDLToken>,
+    position: usize,
+}
+
+impl DDLParser {
+    fn new(tokens: Vec<DDLToken>) -> Self {
+        Self {
+            tokens,
+            position: 0,
+        }
+    }
+
+    fn parse_model(&mut self) -> Option<Model> {
+        if self.position >= self.tokens.len() {
+            return None;
+        }
+        let mut model = Model {
+            name: String::new(),
+            fields: Vec::new(),
+        };
+
+        let mut token = &self.tokens[self.position];
+        if token.token_type != DDLTokenType::Model {
+            return None;
+        }
+        self.position += 1;
+
+        token = &self.tokens[self.position];
+        match &token.token_type {
+            DDLTokenType::Identifier(name) => {
+                model.name = name.clone();
+            }
+            _ => return None,
+        }
+        self.position += 1;
+
+        token = &self.tokens[self.position];
+        if token.token_type != DDLTokenType::OpenBrace {
+            return None;
+        }
+        self.position += 1;
+
+        while self.position < self.tokens.len() {
+            let field = self.parse_field();
+            if let Some(field) = field {
+                model.fields.push(field);
+            } else {
+                break;
+            }
+        }
+
+        Some(model)
+    }
+
+    fn parse_field(&mut self) -> Option<Field> {
+        if self.position >= self.tokens.len() {
+            return None;
+        }
+        let mut field = Field {
+            name: String::new(),
+            field_type: FieldType::Int,
+            is_nullable: false,
+            is_primary_key: false,
+            is_foreign_key: false,
+            references: None,
+        };
+
+        let mut token = &self.tokens[self.position];
+        match &token.token_type {
+            DDLTokenType::Identifier(name) => {
+                field.name = name.clone();
+            }
+            _ => return None,
+        }
+        self.position += 1;
+
+        token = &self.tokens[self.position];
+        match &token.token_type {
+            DDLTokenType::Type(t) => {
+                field.field_type = match &t[..] {
+                    "Int" => FieldType::Int,
+                    "Float" => FieldType::Float,
+                    "String" => FieldType::String,
+                    "Date" => FieldType::Date,
+                    _ => FieldType::Int,
+                };
+            }
+            _ => return None,
+        }
+        self.position += 1;
+
+        token = &self.tokens[self.position];
+        if token.token_type == DDLTokenType::QuestionMark {
+            field.is_nullable = true;
+            self.position += 1;
+        }
+
+        token = &self.tokens[self.position];
+        while token.token_type == DDLTokenType::AtSign {
+            self.position += 1;
+            token = &self.tokens[self.position];
+            if token.token_type == DDLTokenType::Identifier("id".to_string()) {
+                field.is_primary_key = true;
+                self.position += 1;
+            } else if token.token_type == DDLTokenType::Identifier("references".to_string()) {
+                self.position += 1;
+                token = &self.tokens[self.position];
+
+                if !matches!(token.token_type, DDLTokenType::OpenParen) {
+                    return None;
+                }
+
+                let model = match &self.tokens[self.position + 1].token_type {
+                    DDLTokenType::Identifier(id) => id.clone(),
+                    _ => return None,
+                };
+
+                let model_field = match &self.tokens[self.position + 2].token_type {
+                    DDLTokenType::Identifier(id) => id.clone(),
+                    _ => return None,
+                };
+
+                field.references = Some((model, model_field));
+                self.position += 3;
+
+                token = &self.tokens[self.position];
+                if !matches!(token.token_type, DDLTokenType::CloseParen) {
+                    return None;
+                }
+
+                self.position += 1;
+            }
+            token = &self.tokens[self.position];
+        }
+
+        Some(field)
+    }
+}
 
 struct DDLAnalyzer {}
 
@@ -196,6 +350,75 @@ mod tests {
             tokens.push(token.token_type);
         }
         assert_eq!(tokens, correct_sequence);
+    }
+
+    #[test]
+    fn test_ddl_parser() {
+        let ddl = "model Employee { EmployeeID String @id FirstName String LastName String DepartmentID Int? JobTitle String? HireDate Date }";
+        let correct_model = vec![Model {
+            name: "Employee".to_string(),
+            fields: vec![
+                Field {
+                    name: "EmployeeID".to_string(),
+                    field_type: FieldType::String,
+                    is_nullable: false,
+                    is_primary_key: true,
+                    is_foreign_key: false,
+                    references: None,
+                },
+                Field {
+                    name: "FirstName".to_string(),
+                    field_type: FieldType::String,
+                    is_nullable: false,
+                    is_primary_key: false,
+                    is_foreign_key: false,
+                    references: None,
+                },
+                Field {
+                    name: "LastName".to_string(),
+                    field_type: FieldType::String,
+                    is_nullable: false,
+                    is_primary_key: false,
+                    is_foreign_key: false,
+                    references: None,
+                },
+                Field {
+                    name: "DepartmentID".to_string(),
+                    field_type: FieldType::Int,
+                    is_nullable: true,
+                    is_primary_key: false,
+                    is_foreign_key: false,
+                    references: None,
+                },
+                Field {
+                    name: "JobTitle".to_string(),
+                    field_type: FieldType::String,
+                    is_nullable: true,
+                    is_primary_key: false,
+                    is_foreign_key: false,
+                    references: None,
+                },
+                Field {
+                    name: "HireDate".to_string(),
+                    field_type: FieldType::Date,
+                    is_nullable: false,
+                    is_primary_key: false,
+                    is_foreign_key: false,
+                    references: None,
+                },
+            ],
+        }];
+        let mut lexer = DDLLexer::new(ddl.to_string());
+        let mut tokens = Vec::new();
+        while let Some(token) = lexer.next_token() {
+            tokens.push(token);
+        }
+        let mut parser = DDLParser::new(tokens);
+        let mut models = Vec::new();
+        while let Some(model) = parser.parse_model() {
+            models.push(model);
+        }
+        assert_eq!(models, correct_model);
     }
 
     // #[test]
